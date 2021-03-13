fb0f33728363c94a71d4c598b0a6a63476e7ac5d
Sebastian E. Kwiatkowski <sebastian.e.kwiatkowski@outlook.com>
2021-03-13 05:26:12 +0100

    generation/generation/generate-if: The condition part is now generated using generateRootBooleanExpression.

--- src/generation/condition/generate-if.js
+++ src/generation/condition/generate-if.js
@@ -1,10 +1,10 @@
 import {concat} from 'standard-functions'
 import generateExpression from '../generate-expression'
-import {generateComparison} from '../boolean/generate-comparison'
+import {generateRootBooleanExpression} from '../boolean/generate-boolean-expression'
 
 export default function generateIf(useAlias) {
     return ({ condition, ifTrue, ifFalse }) => {
-        const [conditionSql, conditionParameters] = generateComparison(useAlias) (condition)
+        const [conditionSql, conditionParameters] = generateRootBooleanExpression(useAlias) (condition)
         const [trueSql, trueParameters] = generateExpression(useAlias) (ifTrue)
         const [falseSql, falseParameters] = generateExpression(useAlias) (ifFalse)
 
